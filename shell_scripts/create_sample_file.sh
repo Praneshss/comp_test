db449eac1ab04231e7d25d2eced3737572adf907
Pranesh Santikellur <pra.net061@gmail.com>
2016-12-21 17:59:49 +0530

    Add files via upload

--- shell_scripts/create_sample_file.sh
+++ shell_scripts/create_sample_file.sh
@@ -1,3 +1,4 @@
+
 if [[ -z "$1" ]] || [[ -z "$2" ]]; then
     echo "Please input filename and size"
     echo "$0  <filename> <size in bytes>"
@@ -12,6 +13,6 @@ else
 filename=$1
 size=$2
 size_cal=$(echo "$size * 3/4  " | bc -l);
-
+	
 openssl rand -out $filename -base64 $size_cal
 fi
